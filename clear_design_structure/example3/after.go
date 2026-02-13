36374f67b5686b261682b739e5cd4215c6f5d96d
aaboyarchukov <boyarchukov30@gmail.com>
2026-02-13 14:28:05 +0300

    refactor example3

--- clear_design_structure/example3/after.go
+++ clear_design_structure/example3/after.go
@@ -3,99 +3,46 @@ package example3
 import (
 	"context"
 	"errors"
-	"time"
 
 	"github.com/google/uuid"
 	"github.com/jackc/pgx/v4"
 )
 
-func (s *Service) CreateApplication(ctx context.Context, application *dto.Application) (uuid.UUID, error) {
-	var applicationID uuid.UUID
-	uploadedFiles := make([]uploadedDoc, 0, len(application.Documents))
+type Document struct {
+	ID   int64
+	Name string
+	Type string
+	File []byte
+}
+
+type CreateApplication struct {
+	InsuranceId     uuid.UUID
+	ClientId        int64
+	ApplicationType ApplicationType
+	Files           []Document
+}
+
+func (s *Service) CreateApplication(ctx context.Context, req CreateApplication) (uuid.UUID, error) {
+	var (
+		applicationID uuid.UUID
+		uploadedDocs  []UploadedDoc
+	)
 
 	err := s.txManager.RunInTx(ctx, func(txCtx context.Context) error {
-		insurance, err := s.insuranceRepo.GetUserInsurance(ctx, application.InsuranceId)
-		if err != nil {
-			return err
-		}
-		application.ProductId = insurance.ProductId
-		application.CustomerId = insurance.CustomerId
 
-		identificationClientId, err := s.identificationRepo.GetClientId(ctx, insurance.CustomerId)
-		if errors.Is(err, pgx.ErrNoRows) {
-			return ErrInsuranceNotFound
-		}
-
+		insurance, appTypeId, err := validateAndGetBaseData(txCtx, req)
 		if err != nil {
 			return err
 		}
 
-		if application.ClientId != identificationClientId {
-			return ErrIdentificationNotFound
-		}
+		application := BuildApplication(insurance, appTypeId)
 
-		if insurance.UUID == uuid.Nil {
-			return ErrInsuranceNotFound
-		}
-
-		appTypeId, err := s.applicationRepo.GetApplicationTypeId(ctx, application.ApplicationType)
-		if err != nil {
-			return err
-		}
-		application.TypeId = int64(appTypeId)
-
-		applicationID = uuid.New()
-
-		for _, d := range application.Documents {
-			key, errCreate := s.storageRepo.Create(
-				ctx,
-				uuid.New().String(),
-				d.Name,
-				d.File,
-				detectContentType(d.File),
-			)
-
-			if errCreate != nil {
-				return errCreate
-			}
-
-			uploadedFiles = append(uploadedFiles, uploadedDoc{
-				Name:  d.Name,
-				Type:  d.Type,
-				S3Key: key,
-			})
-		}
-
-		if err = s.applicationRepo.Create(txCtx, *application, applicationID); err != nil {
-			return err
-		}
-
-		resultDocuments := make([]dto.Document, 0, len(uploadedFiles))
-		for _, u := range uploadedFiles {
-			documentTypeId, errGet := s.documentsRepo.GetDocumentTypeId(ctx, u.Type)
-			if errGet != nil {
-				return errGet
-			}
-
-			resultDocuments = append(resultDocuments, dto.Document{
-				InsuranceID:   &insurance.UUID,
-				ApplicationID: &applicationID,
-				ProductID:     &insurance.ProductId,
-				Name:          u.Name,
-				TypeID:        int32(documentTypeId),
-				S3Link:        u.S3Key,
-				CreatedAt:     time.Now(),
-				ModifiedAt:    time.Now(),
-			})
-		}
-
-		appDocs, err := s.documentsRepo.Create(txCtx, resultDocuments)
+		applicationID, err := s.applicationRepo.Create(txCtx, application)
 		if err != nil {
 			return err
 		}
 
-		err = s.documentsRepo.SaveApplicationDocuments(txCtx, appDocs, applicationID)
-		if err != nil {
+		if err := s.attachDocuments(txCtx, req.ClientId, applicationID, req.Files, uploadedDocs); err != nil {
 			return err
 		}
 
@@ -108,4 +55,64 @@ func (s *Service) CreateApplication(ctx context.Context, application *dto.Applic
 	}
 
 	return applicationID, nil
+}
+
+type AppTypeId = int32
+
+func (s *Service) validateAndGetBaseData(ctx context.Context, req CreateApplication) (dto.Insurance, AppTypeId, error) {
+	insurance, err := s.insuranceRepo.GetUserInsurance(ctx, req.InsuranceId)
+	if err != nil {
+		return dto.Insurance{}, 0, err
+	}
+
+	identificationClientId, err := s.identificationRepo.GetClientId(ctx, insurance.CustomerId)
+	if errors.Is(err, pgx.ErrNoRows) {
+		return dto.Insurance{}, 0, ErrInsuranceNotFound
+	}
+
+	if err != nil {
+		return err
+	}
+
+	if req.ClientId != identificationClientId {
+		return dto.Insurance{}, 0, ErrIdentificationNotFound
+	}
+
+	appTypeId, err := s.applicationRepo.GetApplicationTypeId(ctx, req.ApplicationType)
+	if err != nil {
+		return dto.Insurance{}, 0, err
+	}
+
+	return insurance, appTypeId, nil
+}
+
+func (s *Service) attachDocuments(ctx context.Context, clientID int64, applicationID uuid.UUID, applicationDocs []Document, uploadedFiles []Document) error {
+	clientDocs, err := s.documentsRepo.GetClientDocIdsById(ctx, clientID)
+	if err != nil && !errors.Is(err, pgx.ErrNoRows) {
+		return err
+	}
+
+	documents := DocumentsToRepoContract(applicationDocs)
+	failedFiles := s.storageRepo.CreateBatch(ctx, documents)
+
+	for _, file := range failedFiles {
+		if file.Error != nil {
+			return ErrAttachDocument
+		}
+
+		uploadedFiles = append(uploadedFiles, Document{
+			S3Key:   file.S3Link,
+			DocType: file.FileType,
+		})
+	}
+
+	if err := s.documentsRepo.SaveApplicationDocuments(ctx, applicationID, clientDocs); err != nil {
+		return err
+	}
+
+	if err := s.documentsRepo.SaveApplicationDocuments(ctx, applicationID, uploadedFiles); err != nil {
+		return err
+	}
+
+	return nil
 }
