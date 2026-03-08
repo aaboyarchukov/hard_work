0c71ca4ca3b159618d3ee51713cb30b0db042769
aaboyarchukov <boyarchukov30@gmail.com>
2026-03-08 23:38:28 +0300

    minimize_steps | example 1 - refactor solution - tests passed

--- minimize_steps/example1/solution.go
+++ minimize_steps/example1/solution.go
@@ -1,5 +1,23 @@
 package example1
 
+func BinarySearchLeft(array []int, target int) int {
+	left, right := 0, len(array)-1
+
+	for left < right {
+		middle := (left + right) / 2
+
+		if array[middle] >= target {
+			return middle
+		}
+
+		if array[middle] < target {
+			left = middle + 1
+		}
+	}
+
+	return -1
+}
+
 func StrictlyMonotonousSequence(array []int) []int {
 	size := len(array)
 	if size <= 1 {
@@ -7,5 +25,15 @@ func StrictlyMonotonousSequence(array []int) []int {
 	}
 
 	result := make([]int, 0, size)
+	collection := make([]int, 0, size)
+	for indx := range array {
+		target := array[indx]
+		collection_pos := BinarySearchLeft(collection, target)
+
+		if collection_pos == -1 {
+			collection = append(collection, target)
+		}
+
+	}
 	return result
 }
--- minimize_steps/example1/solution_test.go
+++ minimize_steps/example1/solution_test.go
@@ -5,14 +5,14 @@ import (
 	"testing"
 )
 
-type Case struct {
-	Name   string
-	Input  []int
-	Output []int
-}
-
 func TestStrictlyMonotonousSequence(t *testing.T) {
-	cases := []Case{
+	type strictlyMonotonousSequenceCase struct {
+		Name   string
+		Input  []int
+		Output []int
+	}
+
+	cases := []strictlyMonotonousSequenceCase{
 		{
 			"empty array",
 			[]int{},
@@ -23,18 +23,46 @@ func TestStrictlyMonotonousSequence(t *testing.T) {
 			[]int{1},
 			[]int{1},
 		},
-		// {
-		// 	"odd elements array",
-		// 	[]int{7, 1, 2, 3, 0, 4, 5, 6, 5},
-		// 	[]int{1, 2, 3, 4, 5, 6},
-		// },
+		{
+			"odd elements array",
+			[]int{7, 1, 2, 3, 0, 4, 5, 6, 5},
+			[]int{1, 2, 3, 4, 5, 6},
+		},
 	}
 
 	for _, testCase := range cases {
-		t.Run(testCase.Name, func(tt *testing.T) {
+		t.Run(testCase.Name, func(subT *testing.T) {
 			result := StrictlyMonotonousSequence(testCase.Input)
 			if slices.Compare(result, testCase.Output) != 0 {
-				tt.Fatalf("FAILED: %s, wanted: %v, got: %v", testCase.Name, testCase.Output, result)
+				subT.Fatalf("FAILED: %s, wanted: %v, got: %v", testCase.Name, testCase.Output, result)
+			}
+		})
+	}
+}
+
+func TestBinarySearchLeft(t *testing.T) {
+	type binarySearchLeftCase struct {
+		Name       string
+		InputArray []int
+		Target     int
+		Output     int
+	}
+
+	cases := []binarySearchLeftCase{
+		{
+			"empty array",
+			[]int{},
+			1,
+			-1,
+		},
+	}
+
+	for _, testCase := range cases {
+		t.Run(testCase.Name, func(subT *testing.T) {
+			resultIndx := BinarySearchLeft(testCase.InputArray, testCase.Target)
+
+			if resultIndx != testCase.Output {
+				subT.Fatalf("FAILED: %s, wanted: %v, got: %d", testCase.Name, testCase.Output, resultIndx)
 			}
 		})
 	}
